1595313d402995269e98f900f3b469521bcd2728
Rathalin <danielflo1997@hotmail.com>
2023-02-06 11:00:10 +0100

    Add sort and filter to book service

--- angular-micro-frontends/src/libs/book/src/lib/book.service.ts
+++ angular-micro-frontends/src/libs/book/src/lib/book.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@angular/core'
 import { BookContentType } from '@angular-micro-frontends/type-definitions'
 import { CollectionService } from './collection.service'
+import * as qs from 'qs'
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService extends CollectionService {
   public queryBooks() {
-    return this.query<BookContentType[]>('/books')
+    const query = qs.stringify({
+      sort: 'title',
+      populate: ['authors'],
+    })
+    return this.query<BookContentType[]>(`/books?${query}`)
+  }
+
+  public filterBooks(search: string) {
+    const query = qs.stringify({
+      sort: 'title',
+      populate: ['authors'],
+      filters: {
+        title: {
+          $containsi: search,
+        },
+      },
+    })
+    return this.query<BookContentType[]>(`/books?${query}`)
   }
 }
