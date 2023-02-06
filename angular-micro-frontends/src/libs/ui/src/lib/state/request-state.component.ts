d45e7e61521468a81a11ec1bb9e2f5c0b5d6fb23
Rathalin <danielflo1997@hotmail.com>
2023-02-06 10:01:40 +0100

    Implement request state component

--- angular-micro-frontends/src/libs/ui/src/lib/state/request-state.component.ts
+++ angular-micro-frontends/src/libs/ui/src/lib/state/request-state.component.ts
@@ -1,9 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { RequestState } from '@angular-micro-frontends/types'
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'ui-request-state',
-  template: ` <p>request-state works!</p> `,
+  template: `
+    <ng-container *ngIf="state != null">
+      <ui-loading *ngIf="state.isLoading" [text]="loadingText"></ui-loading>
+      <ui-success *ngIf="state.isSuccess" [text]="successText"></ui-success>
+      <ui-error *ngIf="state.isError" [text]="errorText"></ui-error>
+    </ng-container>
+  `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RequestStateComponent {}
+export class RequestStateComponent {
+  @Input() state: RequestState<unknown> | null = null
+  @Input() loadingText = 'Loading'
+  @Input() successText = 'Success'
+  @Input() errorText = 'Error'
+}
