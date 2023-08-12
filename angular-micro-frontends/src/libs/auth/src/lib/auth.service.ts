19c73148e81980162fba2e3d2de0483e711a3021
Rathalin <danielflo1997@hotmail.com>
2023-08-12 15:07:33 +0200

    Auth service obervables stuff

--- angular-micro-frontends/src/libs/auth/src/lib/auth.service.ts
+++ angular-micro-frontends/src/libs/auth/src/lib/auth.service.ts
@@ -32,7 +32,7 @@ export class AuthService {
   private authSubject = new BehaviorSubject<Authentication | null>(null)
   auth$ = this.authSubject.asObservable()
   isAuthenticated$ = this.auth$.pipe(map((auth) => auth != null))
-  private readonly storage = localStorage
+  private readonly storage = sessionStorage
   private readonly storageTokenKey = 'auth'
   private readonly storageUserKey = 'user'
 
@@ -97,16 +97,20 @@ export class AuthService {
 
   logout() {
     this.authSubject.next(null)
+    this.storage.removeItem(this.storageTokenKey)
+    this.storage.removeItem(this.storageUserKey)
   }
 
   private loadFromStorage() {
-    const cachedToken = this.storage.getItem(this.storageTokenKey)
-    const cachedUser = this.storage.getItem(this.storageUserKey)
-    if (cachedToken != null && cachedUser != null) {
+    const cachedTokenString = this.storage.getItem(this.storageTokenKey)
+    const cachedUserString = this.storage.getItem(this.storageUserKey)
+    if (cachedTokenString != null && cachedUserString != null) {
+      const cachedUser = JSON.parse(cachedUserString)
       this.authSubject.next({
-        jwt: cachedToken,
-        user: JSON.parse(cachedUser),
+        jwt: cachedTokenString,
+        user: cachedUser,
       })
+      this.currentUserSubject.next(cachedUser)
     }
   }
 
